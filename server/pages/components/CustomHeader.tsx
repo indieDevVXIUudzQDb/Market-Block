25c07f0a0bae02dec58c1c5e389824507dd587d4
indieDevVXIUudzQDb <tokyoIndieDev@outlook.com>
2022-05-05 19:28:40 +0900

    add mantine component library

---
+++ server/pages/components/CustomHeader.tsx
@@ -0,0 +1,7 @@
+import {Header} from "@mantine/core";
+
+export const CustomHeader = () => {
+  return(
+      <Header height={60} p="xs">{/* Header content */}</Header>
+  )
+}
---
+++ server/pages/components/CustomNavbar.tsx
@@ -0,0 +1,7 @@
+import {Navbar} from "@mantine/core";
+
+export const CustomNavbar = () => {
+    return(
+        <Navbar width={{ base: 300 }} height={500} p="xs">{}</Navbar>
+    )
+}
